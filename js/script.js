89e8409c54325eff94a9f508c7c5c8d78f85710f
rustymotor <vmfpdlasz@gmail.com>
2024-02-07 10:02:05 +0900

    a

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// vanilla script 아무것도 첨가되지 않은 자바스크립트라는 뜻
+const pop = document.querySelector('.popup');
+const btn = document.querySelector('.popup>span');
+btn.onclick=function(){
+    pop.style.display="none";
+} 
